a6a764ecabf0e4cbc989c89f65dfc7ecf143867e
Aspect <41125780+Aspect12@users.noreply.github.com>
2024-04-16 00:40:32 +0300

    User Registration assignment.

---
+++ Java/DatabaseUserRegistration/src/databaseuserregistration/DBCredentials.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package databaseuserregistration;
+
+/**
+ *
+ * @author xenoc
+ */
+public class DBCredentials {
+    static final String USERNAME = "root";
+    static final String PASSWORD = "";
+    static final String CONN_STRING = "jdbc:mysql://localhost:3306/registeredusers";
+}
---
+++ Java/DatabaseUserRegistration/src/databaseuserregistration/DatabaseUserRegistration.java
@@ -0,0 +1,10 @@
+
+package databaseuserregistration;
+
+public class DatabaseUserRegistration {
+    public static void main(String[] args) {       
+        LoginForm loginForm = new LoginForm();
+        loginForm.setLocationRelativeTo(null);
+        loginForm.setVisible(true);
+    }
+}
