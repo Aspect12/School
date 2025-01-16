b1184b87f130b9725f4cd44046004769e2b49d8b
Aspect <xenochris12@gmail.com>
2025-01-16 14:27:14 +0200

    Add encryption program 2.

--- Encryption Program/encryption2.cpp
+++ Encryption Program/encryption2.cpp
@@ -14,7 +14,7 @@ int main() {
 	int blockSize;
 	string key;
 
-	// Ask for input
+ 	// Ask for input
 	while (true) {
 		cout << "Enter the message: ";
 		getline(cin, message);
@@ -41,10 +41,18 @@ int main() {
 		cout << "Invalid block size. Please enter a positive integer." << endl;
 	}
 
-	if (blockSize > message.length()) {
-		blockSize = message.length();
+	if (blockSize > message.length() || blockSize > 9) {
+		int max;
+		
+		if (message.length() <= 9) {
+			max = message.length();
+		} else {
+			max = 9;
+		}
 
-		cout << "Block size out of bounds; clamping to " << blockSize << "." << endl;
+		blockSize = max;
+
+		cout << "Block size out of bounds; clamping to " << max << "." << endl;
 	}
 
 	while (true) {
@@ -54,23 +62,51 @@ int main() {
 		bool valid = true;
 
 		if (key.length() != blockSize) {
-			valid = false;
 			cout << "Key length must be equal to block size." << endl;
 		} else {
-			for (char c : key) {
-				if (isdigit(c)) continue;
+			vector<bool> seen(blockSize + 1, false);
 			
-				valid = false;
-				cout << "Key must be a positive integer." << endl;
+			for (char c : key) {
+				if (!isdigit(c)) {
+					valid = false;
 
-				break;
+					cout << "Each character of the key must be a digit." << endl;
+
+					break;
+				}
+
+				if ((c - '0') > blockSize || (c - '0') <= 0) {
+					valid = false;
+
+					cout << "Each digit of the key must be a positive integer between 1 and the block size." << endl;
+				
+					break;
+				}
+
+				if (seen[c - '0']) {
+					valid = false;
+
+					cout << "Each digit of the key must be unique." << endl;
+
+					break;
+				}
+
+				seen[c - '0'] = true;
 			}
-		}
 
-		if (valid) break;
+			if (valid) break;
+		}
 
 		cin.clear();
 		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	}
+
+	// Reverse the key
+	for (int i = 0; i < key.length() / 2; i++) {
+		char temp = key[i];
+		
+		key[i] = key[key.length() - i - 1];
+		key[key.length() - i - 1] = temp;
 	}
 
 	cout << "Original message: " << message << endl;
@@ -103,21 +139,21 @@ string encrypt(string message, int blockSize, string key) {
 	vector<string> blocks;
 
 	for (int i = 0; i < message.length(); i += blockSize) {
-		blocks.push_back(message.substr(i, blockSize));
+		string block = message.substr(i, blockSize);
+
+		if (block.length() < blockSize) {
+			block.append(blockSize - block.length(), 'Z');
+		}
+
+		blocks.push_back(block);
 	}
 
 	// Move
 	for (int i = 0; i < blocks.size(); i++) {
-		for (int j = 0; j < blocks[i].length(); j++) {
-			int shift = key[j] - '0';
+		string block = blocks[i];
 
-			for (int k = 0; k < shift; k++) {
-				if (blocks[i][j] == 'Z') {
-					blocks[i][j] = 'A';
-				} else {
-					blocks[i][j]++;
-				}
-			}
+		for (int j = 0; j < block.length(); j++) {
+			blocks[i][j] = block[key[j] - '1'];
 		}
 	}
 
@@ -141,16 +177,10 @@ string decrypt(string message, int blockSize, string key) {
 
 	// Move
 	for (int i = 0; i < blocks.size(); i++) {
-		for (int j = 0; j < blocks[i].length(); j++) {
-			int shift = key[j] - '0';
+		string block = blocks[i];
 
-			for (int k = 0; k < shift; k++) {
-				if (blocks[i][j] == 'A') {
-					blocks[i][j] = 'Z';
-				} else {
-					blocks[i][j]--;
-				}
-			}
+		for (int j = 0; j < block.length(); j++) {
+			blocks[i][key[j] - '1'] = block[j];
 		}
 	}
 
